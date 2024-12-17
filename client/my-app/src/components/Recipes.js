afc69f73788f235be5802b121df8d4fc16aa63d8
Ritul <01fe22bca126@kletech.ac.in>
2024-12-17 16:35:05 +0530

    Fixed Registers

--- client/my-app/src/components/Recipes.js
+++ client/my-app/src/components/Recipes.js
@@ -196,7 +196,7 @@ import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../styles/RecipeStyle.css";
-import SearchBar from "./components/SearchBar";
+import SearchBar from "./SearchBar.js";
 
 const RecipeCard = ({ recipe, onDelete, onAddToFavorites }) => {
   const [showIngredients, setShowIngredients] = useState(false);
